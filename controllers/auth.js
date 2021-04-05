37546e12280ed4610209e7f98cb6fdce2b296001
domouling <gilberto.domoulin@gmail.com>
2021-04-05 19:49:58 -0400

    Google Sign In Added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -51,6 +52,59 @@ const login = async(req, res = response) => {
 }
 
 
+const googleSignin = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            //Crear Usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si usuario BD status
+        if(!usuario.status){
+            return res.status(401).json({
+                msg: 'Algo a Ocurrido.. Hable con el Administrador - Usuario Bloqueado'
+            });
+        }
+
+        // Generar su JWT
+        const token = await generarJWT(usuario.id);
+        
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch(err) {
+
+        res.status(400).json({
+            msg: 'Token Google no es Reconocido'
+        })
+
+    }
+
+    
+
+}
+
+
 module.exports = {
-    login
+    login,
+    googleSignin
 }
